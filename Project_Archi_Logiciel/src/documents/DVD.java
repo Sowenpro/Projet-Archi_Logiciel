662b94a1273c1240f1f4c67b2f99e6e603f4df48
Sowenpro <cardonsowenpro@gmail.com>
2024-03-06 17:55:49 +0100

    start

---
+++ Project_Archi_Logiciel/src/documents/DVD.java
@@ -0,0 +1,43 @@
+package documents;
+
+import abonnes.Abonne;
+
+public class DVD implements Document {
+
+	@Override
+	public int numero() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Abonne emprunteur() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Abonne reserveur() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void reservationPour(Abonne ab) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void empruntPar(Abonne ab) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void retour() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

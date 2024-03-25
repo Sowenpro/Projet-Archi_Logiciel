a224529610094d6c211f3e4990e7fdf04773f668
Valentin CUENOT <95446216+TheNoNameGuy1@users.noreply.github.com>
2024-03-25 16:49:40 +0100

    fix de l'illegalargumentexception

--- Project_Archi_Logiciel/src/serveurs/Appli.java
+++ Project_Archi_Logiciel/src/serveurs/Appli.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class Appli {
 	static int PORT;
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Choisir un port entre 3000 ou 4000");
 		PORT = sc.nextInt();
 		sc.close();
-		if (PORT!= 3000 || PORT != 4000) {
+		if (!(PORT == 3000 || PORT == 4000)) {
 			throw new IllegalArgumentException("Port indisponible");
 		}
 	}

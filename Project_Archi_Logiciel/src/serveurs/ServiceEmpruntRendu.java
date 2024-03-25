3e52794b2f861ae957c98af792b04a1542765a2f
Valentin CUENOT <95446216+TheNoNameGuy1@users.noreply.github.com>
2024-03-25 16:45:45 +0100

    J'ai effacé les erreurs

--- Project_Archi_Logiciel/src/serveurs/ServiceEmpruntRendu.java
+++ Project_Archi_Logiciel/src/serveurs/ServiceEmpruntRendu.java
@@ -7,6 +7,12 @@ public class ServiceEmpruntRendu implements Runnable {
 
 	public ServiceEmpruntRendu(Socket accept) {
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'run'");
 	}	
 
 }

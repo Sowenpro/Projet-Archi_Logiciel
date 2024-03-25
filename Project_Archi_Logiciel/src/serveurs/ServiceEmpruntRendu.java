90946ece2169c91fc8e235a330d10870d5938f27
Sowenpro <cardonsowenpro@gmail.com>
2024-03-25 16:38:49 +0100

    test

---
+++ Project_Archi_Logiciel/src/serveurs/ServiceEmpruntRendu.java
@@ -0,0 +1,12 @@
+package serveurs;
+
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ServiceEmpruntRendu implements Runnable {
+
+	public ServiceEmpruntRendu(Socket accept) {
+		// TODO Auto-generated constructor stub
+	}	
+
+}

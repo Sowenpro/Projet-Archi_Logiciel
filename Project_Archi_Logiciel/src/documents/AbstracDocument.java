fb67e51e0ffb8a61e5ead7aebe8a9e688059546e
TheNoNameGuy1 <95446216+TheNoNameGuy1@users.noreply.github.com>
2024-03-26 11:16:45 +0100

    Création des documents

---
+++ Project_Archi_Logiciel/src/documents/AbstracDocument.java
@@ -0,0 +1,62 @@
+package documents;
+
+import abonnes.Abonne;
+
+public class AbstracDocument implements Document {
+    private static int cpt = 0;
+    protected int num;
+    protected String nom;
+    protected Abonne emprunteur;
+    protected Abonne reserveur;
+
+    public AbstracDocument(String nom) {
+        num = cpt++;
+        this.nom = nom;
+        reserveur = null;
+        emprunteur = null;
+    }
+
+    @Override
+    public int numero() {
+        return num;
+    }
+
+    public String toString() {
+        return nom;
+    }
+
+    @Override
+    public Abonne emprunteur() {
+        return emprunteur;
+    }
+
+    @Override
+    public Abonne reserveur() {
+        return reserveur;
+    }
+
+    @Override
+    public void reservationPour(Abonne ab) {
+        if (reserveur != null) {
+            throw new IllegalStateException("Impossible de réserver un document déjà réservé");
+        } else if (emprunteur != null) {
+            throw new IllegalStateException("Impossible de réserver un document emprunté");
+        }
+        reserveur = ab;
+    }
+
+    @Override
+    public void empruntPar(Abonne ab) {
+        if (emprunteur != null) {
+            throw new IllegalStateException("Impossible d'emprunter un document déjà emprunté");
+        }
+        emprunteur = ab;
+        reserveur = null;
+    }
+
+    @Override
+    public void retour() {
+        reserveur = null;
+        emprunteur = null;
+    }
+}
--- Project_Archi_Logiciel/src/documents/Livre.java
+++ Project_Archi_Logiciel/src/documents/Livre.java
@@ -1,43 +1,14 @@
 package documents;
 
-import abonnes.Abonne;
+public class Livre extends AbstracDocument {
+	private int nbPages;
 
-public class Livre implements Document {
-
-	@Override
-	public int numero() {
-		// TODO Auto-generated method stub
-		return 0;
+	public Livre(String nom, int nbPages) {
+		super(nom);
+		this.nbPages = nbPages;
 	}
 
-	@Override
-	public Abonne emprunteur() {
-		// TODO Auto-generated method stub
-		return null;
+	public int nbPages() {
+		return nbPages;
 	}
-
-	@Override
-	public Abonne reserveur() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void reservationPour(Abonne ab) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void empruntPar(Abonne ab) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void retour() {
-		// TODO Auto-generated method stub
-
-	}
-
 }

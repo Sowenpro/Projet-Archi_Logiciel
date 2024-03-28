5ce4fc2b0fed910193c224acf767f2f5154fe112
Valentin CUENOT <95446216+TheNoNameGuy1@users.noreply.github.com>
2024-03-28 13:54:42 +0100

    Quick fix sur l'Appli

--- Project_Archi_Logiciel/src/serveurs/Appli.java
+++ Project_Archi_Logiciel/src/serveurs/Appli.java
@@ -6,41 +6,54 @@ import abonnes.Abonne;
 import documents.Document;
 
 public class Appli {
-	static int PORT;
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Choisir un port entre 3000 ou 4000");
-		PORT = sc.nextInt();
-		sc.close();
-		if (!(PORT == 3000 || PORT == 4000)) {
+		int port = sc.nextInt();
+		if (!(port == 3000 || port == 4000)) {
+			sc.close();
 			throw new IllegalArgumentException("Port indisponible");
 		}
-		else if (PORT==4000) {
-			System.out.println("Voulez-vous emprunter ou retourner ?");
-			String reponse = sc.next();
-			if (reponse=="emprunter") {
-				System.out.print( "Veuillez saisir votre numero d'abonne: " );
-				String tempString1 = sc.nextLine();
-				System.out.print( "Veuillez saisir le numero du document: " );
-				String tempString2 = sc.nextLine();
-				sc.close();
-				//Document.emprunterPar(tempString1,tempString2);
-			}
-			else if (reponse=="retourner") {
-				System.out.print( "Veuillez saisir le numero du document: " );
-				String tempString = sc.nextLine();
-				sc.close();
-				//Document.retour(tempString);
+		else if (port == 4000) {
+			while (true) {
+				System.out.println("Voulez-vous emprunter ou retourner ? (e/r)");
+				String reponse = sc.next();
+				if (reponse.compareTo("e") == 0) {
+					System.out.print( "Veuillez saisir votre numero d'abonne: " );
+					int tempString1 = sc.nextInt();
+					System.out.println("Quel document voulez-vous retourner ?");
+					String tempString2 = sc.next();
+					sc.close();
+					//Document.emprunterPar(tempString1,tempString2);
+					return;
+				}
+				else if (reponse.compareTo("r") == 0) {
+					System.out.print( "Veuillez saisir le numero du document: " );
+					String tempString = sc.nextLine();
+					sc.close();
+					//Document.retour(tempString);
+					return;
+				}
+				while (true) {
+					System.out.println("Commande inconnue : voulez-vous quitter l'application ? (y/n)");
+					reponse = sc.next();
+					if (reponse.compareTo("y") == 0) {
+						sc.close();
+						return;
+					} else if (reponse.compareTo("n") == 0) {
+						break;
+					}
+				}
 			}
 		}
-		else if (PORT==3000) {
+		else if (port == 3000) {
 			System.out.print( "Veuillez saisir votre numero d'abonne: " );
-			String tempString1 = sc.nextLine();
+			int tempString1 = sc.nextInt();
 			System.out.println("Quel document voulez-vous retourner ?");
 			String tempString2 = sc.next();
 			sc.close();
 			//Document.reservationPour(tempString1,tempString2);
+			return;
 		}
 	}
 }

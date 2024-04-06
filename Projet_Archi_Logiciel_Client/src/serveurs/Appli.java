ea12b427772dd8a09518463755566a0b5d5982ed
Valentin CUENOT <95446216+TheNoNameGuy1@users.noreply.github.com>
2024-04-06 14:23:01 +0200

    J'ai fix l'appli client pour qu'elle fonctionne correctement

--- Projet_Archi_Logiciel_Client/src/serveurs/Appli.java
+++ Projet_Archi_Logiciel_Client/src/serveurs/Appli.java
@@ -29,12 +29,10 @@ public class Appli {
 						System.out.println("Veuillez saisir le numéro du document: ");
 						String empruntdoc = sc.next();
 						out.writeUTF(empruntdoc);
-						sc.close();
 						DataInputStream in = new DataInputStream(client.getInputStream());
 						String msgconfirmation = in.readUTF();
 						System.out.println(msgconfirmation);
-
-					} 
+					}
 					else if (reponse.compareTo("r") == 0) {
 						DataOutputStream out1 = new DataOutputStream(client.getOutputStream());
 						System.out.println("Vous avez choisi de retourner");
@@ -47,63 +45,65 @@ public class Appli {
 						System.out.println(msgconfirmation);
 					} 
 					else if (reponse.compareTo("q") == 0) {
-						System.out.println("Voulez-vous vraiment quitter l'application ? (y/n)");
-						String rep = sc.next();
-						rep = sc.next();
-						if (rep.compareTo("y") == 0) {
-							out.writeUTF(rep);
-							sc.close();
-
-						} 
-						else if (rep.compareTo("n") == 0) {
-							break;
+						while (true) {
+							System.out.println("Voulez-vous vraiment quitter l'application ? (y/n)");
+							String rep = sc.next();
+							if (rep.compareTo("y") == 0) {
+								out.writeUTF(rep);
+								sc.close();
+								return;
+							}
+							else if (rep.compareTo("n") == 0) {
+								break;
+							}
+							System.out.println("Commande inconnue, veuillez réessayer.");
 						}
 					}
-				}
-				while (true) {
-					System.out.println("Commande inconnue, veuillez réessayer.");
+					else {
+						System.out.println("Commande inconnue, veuillez réessayer.");
+					}
 				}
 			}
 		}
 		else if (port == 3000) {
 			while (true) {
 				// Connection au serveur Emprunt/Retour
-				try (
-				Socket reservation = new Socket("localhost", port)) {
+				try (Socket reservation = new Socket("localhost", port)) {
 					DataOutputStream out = new DataOutputStream(reservation.getOutputStream());
-					System.out.println("Bienvenue dans le mode réservation, voulez-vous réserver ou quitter ? (r/q)");
+					System.out.println("Voulez-vous réserver ou quitter ? (r/q)");
 					String reponse = sc.next();
-					if (reponse.compareTo("e") == 0) {
+					if (reponse.compareTo("r") == 0) {
 						System.out.print("Veuillez saisir votre numéro d'abonné: ");
 						int numAb = sc.nextInt();
 						out.writeInt(numAb);
 						System.out.print("Veuillez saisir le numéro du document: ");
 						String reservationdoc = sc.next();
 						out.writeUTF(reservationdoc);
-						sc.close();
 						DataInputStream in = new DataInputStream(reservation.getInputStream());
 						String msgconfirmation = in.readUTF();
 						System.out.println(msgconfirmation);
 					}
 					else if (reponse.compareTo("q") == 0) {
-						System.out.println("Voulez-vous vraiment quitter l'application ? (y/n)");
-						String rep = sc.next();
-						rep = sc.next();
-						if (rep.compareTo("y") == 0) {
-							out.writeUTF(rep);
-							sc.close();
-
-						} 
-						else if (rep.compareTo("n") == 0) {
-							break;
+						while (true) {
+							System.out.println("Voulez-vous vraiment quitter l'application ? (y/n)");
+							String rep = sc.next();
+							if (rep.compareTo("y") == 0) {
+								out.writeUTF(rep);
+								sc.close();
+								return;
+							} 
+							else if (rep.compareTo("n") == 0) {
+								break;
+							}
+							System.out.println("Commande inconnue, veuillez réessayer.");
 						}
 					}
-				}
-				while (true) {
-					System.out.println("Commande inconnue, veuillez réessayer.");
+					else {
+						System.out.println("Commande inconnue, veuillez réessayer.");
+					}
 				}
 			}
-		} 
+		}
 		else {
 			sc.close();
 			throw new IllegalArgumentException("Port indisponible");
